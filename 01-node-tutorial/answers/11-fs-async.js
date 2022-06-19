389c6ead84f130d9b674f7f67c4d80cfa8b51582
Alena <83471057+alenamedved@users.noreply.github.com>
2022-06-19 15:06:50 -0600

    repeat

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -1,29 +1,30 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile } = require("fs");
 
-console.log('start')
-readFile('./content/first.txt', 'utf8', (err, result) => {
+console.log('start');
+
+readFile("./content/first.txt", "utf8", (err, result) => {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  const first = result
-  readFile('./content/second.txt', 'utf8', (err, result) => {
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    const second = result
+    const second = result;
     writeFile(
-      './content/result-async.txt',
-      `Here is the result : ${first}, ${second}`,
+      "./content/result-async.txt",
+      `Here is the result ${first}, ${second}. Text async `,
       (err, result) => {
         if (err) {
-          console.log(err)
-          return
+          console.log(err);
+          return;
         }
-        console.log('done with this task')
+        console.log('done with this task');
       }
-    )
-  })
-})
-console.log('starting next task')
+    );
+  });
+});
+console.log('starting next task');
