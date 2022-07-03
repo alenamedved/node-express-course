0db96effa077d2276a4a603ad08e7af181fe3274
Alena <83471057+alenamedved@users.noreply.github.com>
2022-07-03 11:08:02 -0600

    wip

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,6 @@
-console.log('Welcome to Node Tutorial')
+const _ = require('lodash')
+
+const item = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(item)
+console.log(newItems);
+console.log("hello people!!!");
